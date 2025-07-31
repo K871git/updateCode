0d0950dd7fb424c3d18331f37ac0a057f0d2dcc9
Spark871 <klm3allemk871champ@gmail.com>
2025-07-31 22:41:50 +0530

    writing the assignmet for python bootcamp day-6

---
+++ assignment/Assignment1.py
@@ -0,0 +1,36 @@
+# 1) Printing the numbers from 1 to 10 using while loop
+
+
+# i=0;
+# while(i<=10):
+#     print(i);
+#     i=i+1;
+
+# 2) PRINT EACH CHARACTER OF THE STRING USING FOR LOOP
+
+# str="string";
+# for i in str:
+#     print(i);
+
+# 3) sum of first n numbers
+
+"""
+n=int(input("Enter the Number for sum ::"));
+
+sum=0;
+i=1;
+while i<=n:
+    sum+=i;
+    i+=1;
+
+print(sum);
+
+"""
+# multiplication table genrator
+n = int(input("Enter the number here :"))
+i = 1
+while i <= 10:
+    print(f"{n} * {i} = {n*i}")
+    i += 1
+print("thank you")
+
